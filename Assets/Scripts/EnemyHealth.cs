93ee71c2978d61735c08627168bd2e8bd9a6e95c
ToastYwasTaken <franz@emoerike.de>
2021-06-13 19:00:35 +0200

    added scripts, GO and stuff
    - added Health, Damage Scripts
    - added Enemy (TODO: model / scripting
    -added GUI (heart display doesn't work yet when using camera scene view)
    -structured the hierachy

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/******************************************************************************
+ * Project: GPA4300Game
+ * File: EnemyHealth.cs
+ * Version: 1.0
+ * Autor: René Kraus (RK); Franz Mörike (FM); Jan Pagel (JP)
+ * 
+ * 
+ * These coded instructions, statements, and computer programs contain
+ * proprietary information of the author and are protected by Federal
+ * copyright law. They may not be disclosed to third parties or copied
+ * or duplicated in any form, in whole or in part, without the prior
+ * written consent of the author.
+ * 
+ * ChangeLog
+ * ----------------------------
+ *  11.06.2021  FM  Created
+ *  
+ *****************************************************************************/
+
+/// <summary>
+/// Updatet die Lebenspunkte des Gegners nach Kollision
+/// </summary>
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]
+    private float ehealth = 100f;    //Startwert
+    public GameObject pdamageObject;
+    private float pdamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pdamage = pdamageObject.GetComponent<Damage>().pDamageProperty;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Bullet")) //tag = "Bullet"
+        {
+            ehealth -= pdamage;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/******************************************************************************
+ * Project: GPA4300Game
+ * File: PlayerHealth.cs
+ * Version: 1.0
+ * Autor: René Kraus (RK); Franz Mörike (FM); Jan Pagel (JP)
+ * 
+ * 
+ * These coded instructions, statements, and computer programs contain
+ * proprietary information of the author and are protected by Federal
+ * copyright law. They may not be disclosed to third parties or copied
+ * or duplicated in any form, in whole or in part, without the prior
+ * written consent of the author.
+ * 
+ * ChangeLog
+ * ----------------------------
+ *  11.06.2021  FM  Created
+ *  
+ *****************************************************************************/
+
+/// <summary>
+/// Updatet die Lebenspunkte des Spielers nach Kollision
+/// </summary>
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private float phealth = 100f;    //Startwert
+    public GameObject edamageObject;
+    private float edamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        edamage = edamageObject.GetComponent<Damage>().eDamageProperty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)  
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))   //tag = "Enemy"
+        {
+            phealth -= edamage;
+        }
+    }
+}
