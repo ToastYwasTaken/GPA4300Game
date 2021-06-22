e701492225588fee2101b673a7cf95e255a581a0
YinJan <85757350+YinJan@users.noreply.github.com>
2021-06-22 16:39:50 +0200

    adaptede default scene

---
+++ Assets/JP/Scripts/TrapDoor_Behavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoor_Behavior : MonoBehaviour
+{
+    [SerializeField]
+    private Animator trapDoorAnimator;
+
+    private void OnTriggerEnter(Collider _other)
+    {
+        if (_other.gameObject.tag == "Player")
+        {
+            trapDoorAnimator.SetTrigger("triggered");
+        }
+    }
+}
---
+++ Assets/JP/Scripts/TrapRocks_Behavior.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapRocks_Behavior : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rockPrefab;
+    [SerializeField]
+    private int numberOfRocks;
+    //[SerializeField]
+    public Transform[] spawnPoints;
+    [SerializeField]
+    private float deflectionForce;
+    [SerializeField]
+    private float timeBetweenSpawns;
+
+    private IEnumerator spawnRocks()
+    {
+        for (int i = 0; i < numberOfRocks; i++)
+        {
+            int spawnPosition = Random.Range(0, spawnPoints.Length);
+            int deflection = Random.Range(0, 4);
+
+            GameObject rock = Instantiate(rockPrefab);
+            rock.transform.Translate(spawnPoints[spawnPosition].position);
+
+            Rigidbody rockRB = rock.GetComponent<Rigidbody>();
+            switch (deflection)
+            {
+                case 0:
+                    rockRB.AddForce(deflectionForce, 0, 0);
+                    break;
+                case 1:
+                    rockRB.AddForce(-deflectionForce, 0, 0);
+                    break;
+                case 2:
+                    rockRB.AddForce(0, 0, deflectionForce);
+                    break;
+                case 3:
+                    rockRB.AddForce(0, 0, -deflectionForce);
+                    break;
+                default:
+                    break;
+            }
+
+
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+    }
+
+    private void OnTriggerEnter(Collider _other)
+    {
+        if (_other.gameObject.tag == "Player")
+        {
+            StartCoroutine(spawnRocks());
+        }
+    }
+}
